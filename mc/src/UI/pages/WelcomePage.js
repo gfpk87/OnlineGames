d055df20bc31c9726be0fee0dc0a59cc66374ac0
gfpk87 <155361515+gfpk87@users.noreply.github.com>
2024-01-07 19:56:57 +0800

    Update WelcomePage.js

--- mc/src/UI/pages/WelcomePage.js
+++ mc/src/UI/pages/WelcomePage.js
@@ -12,16 +12,6 @@ class WelcomePage extends Page {
         this.renderer = null;
     };
     onConnected() {
-        var jumptext = [
-            "Subscribe :D",
-            "Hello world",
-            "Look, a pig!",
-            "Schwarzer Regen"
-        ];
-        var sel = jumptext[Math.floor(Math.random()*4)];
-        // alert(sel);
-        var it = document.getElementById("jumpt");
-        it.innerHTML = sel;
         this.renderer = new WelcomeRenderer(this.bgCanvas);
         this.renderer.play();
         settings.addEventListener("changedValue", this.updateBlur);
